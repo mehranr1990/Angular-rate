7cdbd34d053eacbe439f769a9c7c209df07b385d
mehranr1990 <mehran.r1990@gmail.com>
2024-06-17 18:59:23 +0430

    adding a componenet to show the profit of change coins but not work

--- src/app/components/best-price-modal/best-price-modal.component.ts
+++ src/app/components/best-price-modal/best-price-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, Input, OnInit,  } from '@angular/core';
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
@@ -6,16 +6,20 @@ import { InputTextModule } from 'primeng/inputtext';
 @Component({
   selector: 'app-best-price-modal',
   standalone: true,
-  imports: [DialogModule, ButtonModule, InputTextModule],
+  imports: [DialogModule, ButtonModule, InputTextModule,],
   templateUrl: './best-price-modal.component.html',
   styleUrl: './best-price-modal.component.scss'
 })
-export class BestPriceModalComponent {
+export class BestPriceModalComponent implements OnInit {
   visible: boolean = false;
   position: string = 'center';
-  // @input id:string =''
+  @Input() id :string='' 
   showDialog(position: string) {
       this.position = position; 
       this.visible = true;
+  }
+  ngOnInit() {
+    console.log(this.id);
+    
   }
 }
