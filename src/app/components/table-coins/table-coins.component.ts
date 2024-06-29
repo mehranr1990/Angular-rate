3d699f3804f044895a300ce5f4261002bed685eb
mehran <mehran.r1990@gmail.com>
2024-06-29 08:41:56 +0330

    adding modal for deleting a coin

--- src/app/components/table-coins/table-coins.component.ts
+++ src/app/components/table-coins/table-coins.component.ts
@@ -3,18 +3,20 @@ import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { CoinsService } from '../../core/services/coins.service';
 import { Coin } from '../../core/models/coin.model';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-table-coins',
   standalone: true,
-  imports: [TableModule,ButtonModule],
+  imports: [TableModule, ButtonModule, DialogModule],
   templateUrl: './table-coins.component.html',
   styleUrl: './table-coins.component.scss',
 })
 export class TableCoinsComponent {
   constructor(private coinsService: CoinsService) {}
   coins: Coin[] = [];
-
+  selectedCoin:any=[]
+  visible: boolean = false;
   ngOnInit(): void {
     // this.coinsService.getAll();
     this.coinsService.coins.subscribe({
@@ -23,9 +25,11 @@ export class TableCoinsComponent {
       },
     });
   }
-  deleterow(e:any){
-    this.coinsService.deleteCoin(e).subscribe({next:(resp)=>{
-    }})
-    
+  deleterow() {
+    this.coinsService.deleteCoin(this.selectedCoin.id).subscribe({ next: (resp) => {} });
+  }
+  toggleDialog(e:any) {
+    this.visible = !this.visible;
+    this.selectedCoin = e
   }
 }
